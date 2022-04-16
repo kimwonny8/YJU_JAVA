6e6edc849f67d35cfa30102bab360c9f9542ff7e
kimwonny8 <good3234@naver.com>
2022-04-16 19:19:09 +0900

    study

--- Baekjoon/src/Silver/Q9012.java
+++ Baekjoon/src/Silver/Q9012.java
@@ -1,8 +1,8 @@
-package Main;
+package Silver;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q9012 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -15,14 +15,13 @@ public class Main {
 			for(int j=0; j<txt.length(); j++) {
 				char []arr=new char[txt.length()];
 				arr[j]=txt.charAt(j);
-				if(cnt==0) {
-					if(arr[j]==')') cnt-=2;
-				}
-				else if(arr[j]=='(') cnt++;
+				if(arr[j]=='(') cnt++;
 				else if(arr[j]==')') cnt--;	
+				if(cnt<0) break;
 			} 
 			if(cnt==0) System.out.println("YES");
 			else System.out.println("NO");	
-		} 		
+		} 	
+
 	}
 }
