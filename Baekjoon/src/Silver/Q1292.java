72103dde77bc85b6e622584b89e24682c27d2ac2
kimwonny8 <good3234@naver.com>
2022-05-02 18:13:37 +0900

    20220502

--- Baekjoon/src/Silver/Q1292.java
+++ Baekjoon/src/Silver/Q1292.java
@@ -2,23 +2,27 @@ package Silver;
 
 import java.util.Scanner;
 
-public class DNF_Q1292 {
+public class Q1292 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		//int a=sc.nextInt(), b=sc.nextInt();
-		int [] arr=new int[1001];
+		int a=sc.nextInt(), b=sc.nextInt();
+		int [] arr=new int[1002];
 		int sum=0; 
 		int cnt=1;
 		
-		for(int i=1; i<1001; i++) {
+		for(int i=1; i<=1001; i++) {
 			for(int j=0; j<i; j++) {
+				if(cnt==1001) break;
 				arr[cnt]=i;
 				cnt++;	
 			}
-			System.out.print(arr[i]);
 			}
+		for(int i=a; i<=b; i++) {
+			sum+=arr[i];
 		}
-		
+		System.out.println(sum);
 	}
+		
+}
 
--- GitUp/src/CodeUp/KJW_Q1367.java
+++ GitUp/src/CodeUp/KJW_Q1367.java
@@ -6,9 +6,9 @@ public class KJW_Q1367 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int l=2*n;
+		
 		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=l-i; j++) {
+			for(int j=1; j<=2*n-i; j++) {
 				if(j<=n-i) System.out.print(" ");
 				else  System.out.print("*");
 			}
