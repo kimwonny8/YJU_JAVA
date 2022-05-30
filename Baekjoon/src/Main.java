58528254211f73b69ea157c61d46477b47bd7096
KimJungWon <kimwonny8@gmail.com>
2022-05-30 10:24:56 +0900

    구술 테스트 준비

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
- 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
-	
-	}
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+
+  }
 }
