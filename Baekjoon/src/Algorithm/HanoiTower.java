512c489e0d8e29993656de7e0b2c8ffe4920b7a5
KimJungWon <kimwonny8@gmail.com>
2022-07-19 15:32:26 +0900

    자료구조 수업내용 추가

---
+++ Baekjoon/src/Algorithm/HanoiTower.java
@@ -0,0 +1,21 @@
+package Algorithm;
+
+class HanoiTowerMove {
+	void Move(int num, char start, char mid, char end) {
+		if(num==1) System.out.printf("원반1을 %c에서 %c로 이동\n", start, end);
+		else {
+			Move(num-1, start, end, mid);
+			System.out.printf("원반%d을 %c에서 %c로 이동\n",num, start, end);
+			Move(num-1, mid, start, end);
+		}
+	}
+}
+
+public class HanoiTower {
+
+	public static void main(String[] args) {
+		new HanoiTowerMove().Move(3,'A','B','C');
+	}
+}
+
+
---
+++ Baekjoon/src/Algorithm/Search.java
@@ -0,0 +1,36 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Search {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++) arr[i]=(int)(Math.random()*100);
+		Arrays.sort(arr);
+		System.out.println(BinarySearch(arr, 3));
+	}
+	
+	static int BinarySearch(int[]arr, int target) {
+		int mid, cnt=0;
+		int first=0, last=arr.length-1;
+		
+		while(first<=last) {
+			mid = (first+last)/2;
+			
+			System.out.printf("탐색횟수 %d번, first=%d, last=%d\n",cnt,first,last);
+			if(arr[mid]==target) return mid;
+			else if(arr[mid]>target) {
+				cnt++; last=mid-1; 
+			}
+			else {
+				cnt++; first=mid+1;
+			}
+			// -1, +1 안해주면 무한루프
+		}
+		return -1;
+	}
+}
